bd73fa8b784b8d0d96eaa4fd9ebebe330b3f32a9
Prajit Patil <prajitpatil17@gamil.com>
2017-06-17 20:45:28 +0530

    Adding auto complete feature for custom commands and variables

--- README.md
+++ README.md
@@ -1,21 +1,35 @@
 # Overview
-This is a custom terminal emulator build using [GNU Readline Library](http://cnswww.cns.cwru.edu/php/chet/readline/readline.html). Code is written in Perl, but ideally you can use any language whis has support for above GNU library.
+This is a custom terminal emulator build using [GNU Readline Library](http://cnswww.cns.cwru.edu/php/chet/readline/readline.html). Code is written in Perl(cpan module [Term::ReadLine::Gnu](https://metacpan.org/pod/Term::ReadLine::Gnu) is used), but ideally you can use any language which has support for above GNU library.
 
 # Getting Started
-Use the help command to get the list of featurs.
+Use the help command to get the list of feature.
 ```
 >> help
 ```
 
-# Featues
+# Features
 ## Variables
-This allows a macro like feature where you can set name to any arbitary string and use the name as a subtitute for string in command line.
+This allows a macro like feature where you can set name to any arbitrary string and use the name as a substitute for string in command line.
 ### Example
 ```
 >> set proj_dir /home/john/projects/
 ```
-this will set 'proj_dir' to '/home/john/projects/', now we can use this variable name 'proj_dir' in command line as a subtitute for '/home/john/projects/'
+this will set 'proj_dir' to '/home/john/projects/', now we can use this variable name 'proj_dir' in command line as a substitute for '/home/john/projects/'
 ```
 >> ls $proj_dir
 ```
 Note '$' prefix indicate that its variable name and will be expanded if variable with similar name exist.
+
+## Auto complete
+This allows auto completing custom commands and variable names using '<tab>'.
+### Example
+* Auto complete commands
+```
+>> se<tab>
+```
+this will list all custom commands starting with prefix 'se'.
+* Auto complete variables
+```
+>> ls $fo<tab>
+```
+this will list all custom variables starting with prefix 'fo'.
--- my_terminal.pl
+++ my_terminal.pl
@@ -66,8 +66,13 @@ my %commands = (
 );
 
 my $term = Term::ReadLine->new('My Terminal');
-$term->ornaments(0);    # Remove underline from promt string
-while(1){
+$term->ornaments(0);    # Remove underline from prompt string
+
+# The completion logic is referred from 'http://www.perlmonks.org/?node_id=629849'
+my $term_attr = $term->Attribs;
+$term_attr->{attempted_completion_function} = \&complete;
+
+while(1) {
     my $cmd = $term->readline('>> ');
     $cmd = expand_variables($cmd);
     my ($cmd_name, @cmd_args) = split(' ', $cmd);
@@ -79,8 +84,63 @@ while(1){
     }
 }
 
+sub complete {
+    my ($text, $line, $start, $end) = @_;
+
+    # Example, let's say user typed: 'ls $fo'
+    #          $line = 'ls $fo'
+    #          $text = 'fo'
+    #          $start = 4
+    #          $end   = 6
+
+    my @words = split(' ', $line);
+    my $last_word = $words[-1];         # For above example this will be '$fo'
+
+    if($start == 0){
+        # First word should be a command
+        return $term->completion_matches($text,\&complete_command);
+    }
+    elsif($last_word =~ /^\$/){
+        # If last word start with '$', it can be a variable
+        return $term->completion_matches($text,\&complete_variable);
+    }
+    # Else this will do default to file name completion
+    return;
+}
+
+sub complete_variable {
+    return complete_keyword(@_, [sort keys %variables]);
+}
+
+sub complete_command {
+    return complete_keyword(@_, [sort keys %commands]);
+}
+
+{
+    my $i;
+    my @keywords;
+    sub complete_keyword {
+        my ($text, $state, $keyword_list) = @_;
+        return unless $text;
+        if($state) {
+            $i++;
+        }
+        else { # first call
+            @keywords = @{$keyword_list};
+            $i = 0;
+        }
+        for (; $i<=$#keywords; $i++) {
+            return $keywords[$i] if $keywords[$i] =~ /^\Q$text/;
+        };
+        return undef;
+    }
+}
+
 sub expand_variables {
     my $cmd = shift;
+    # NOTE: 'reverse' & 'sort' make sure we always substitute biggest string first,
+    #       this make sure prefix does not get priority,
+    #       example: if we have 2 variables 'dir' & 'dir1', 'dir1' will be substituted first.
     for my $key (reverse sort keys %variables) {
         $cmd =~ s/\$\Q$key\E/$variables{$key}/gc;
     }
